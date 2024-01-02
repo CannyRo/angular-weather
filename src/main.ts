54e0c3053a69b057112a6876f75a952727bcb3fe
CannyRo <whatcannydev@gmail.com>
2024-01-02 17:10:09 +0100

    creates components

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,30 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Routes, provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  {
+    path: 'home', 
+    title: 'METEO Home', 
+    loadComponent: ()=> import("./app/home/home/home.component").then(module => module.HomeComponent)
+  },
+  {
+    path: ':city',
+    loadComponent: ()=> import("./app/detail/detail.component").then(module => module.DetailComponent)
+  },
+  {
+    path: '**',
+    title: 'Meteo Not Found',
+    loadComponent: ()=> import("./app/page-not-found/page-not-found.component").then(module => module.PageNotFoundComponent)
+  }
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(routes)
+  ]
+})
   .catch((err) => console.error(err));
