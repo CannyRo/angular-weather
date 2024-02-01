842ebdbf83642f72c6a6680534a7612bfd55c963
CannyRo <whatcannydev@gmail.com>
2024-02-01 18:49:53 +0100

    local storage

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -9,21 +9,25 @@ export class LocalStorageService {
 
   // Set a value in local storage
   setItem(key: string, value: string): void {
+    console.log("Save data in Local Storage");
     localStorage.setItem(key, value);
   }
 
   // Get a value from local storage
   getItem(key: string): string | null {
+    console.log("Get data from Local Storage");
     return localStorage.getItem(key);
   }
 
   // Remove a value from local storage
   removeItem(key: string): void {
+    console.log("Delete data in Local Storage");
     localStorage.removeItem(key);
   }
 
   // Clear all items from local storage
   clear(): void {
+    console.log("Delete all data in Local Storage");
     localStorage.clear();
   }
   
