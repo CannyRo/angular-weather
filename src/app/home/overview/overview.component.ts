ebcb31b1f3de6bfe00e5695dff09daffdaf1fd8b
CannyRo <whatcannydev@gmail.com>
2024-02-10 23:12:21 +0100

    fix background day and night

--- src/app/home/overview/overview.component.ts
+++ src/app/home/overview/overview.component.ts
@@ -16,8 +16,15 @@ export class OverviewComponent implements OnChanges{
   @Input() city!: City | null;
   @Input() weather!: Weather | null;
 
+  backgroundColor: string | undefined;
+  textColor: string | undefined;
+
   ngOnChanges(): void {
     console.log("OnChanges => the city is : ", this.city);
     console.log("OnChanges => the weather is : ", this.weather);
+    if(this.weather){
+      this.backgroundColor = this.weather.current.is_day == 0 ? 'bg_night' : 'bg_day';
+      this.textColor = this.weather.current.is_day == 0 ? 'color_night' : 'color_day';
+    }
   }
 }
